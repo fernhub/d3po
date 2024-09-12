e4adf2c14e575f060a0075eb2a2e281fd0608648
Zackary Fernandez <fernandez.zack@icloud.com>
2024-09-12 14:53:15 -0700

    changing env variables config for dev/prod build fix

--- apps/server/src/config.ts
+++ apps/server/src/config.ts
@@ -1,21 +1,25 @@
 import dotenv from "dotenv";
-const result = dotenv.config();
+dotenv.config();
 
-if (result.error) {
-  throw result.error;
-}
-
-const { parsed: envs } = result;
-export const env = { ...envs };
-
-Object.assign(env, {
+export const env = {
   POSTGRES_USER: process.env.POSTGRES_USER,
   POSTGRES_URL: process.env.POSTGRES_URL,
   POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_PORT: process.env.POSTGRES_PORT,
   POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
   API_URL: process.env.API_URL,
   PORT: process.env.PORT,
-  CLIENT_PORT: process.env.CLIENT_PORT,
-});
+  CLIENT_URL: process.env.NODE_ENV == "development" ? "localhost:5173" : process.env.CLIENT_URL,
+  NODE_ENV: process.env.NODE_ENV,
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
+  MISTRAL_API_KEY: process.env.MISTRAL_API_KEY,
+  JWT_SECRET: process.env.JWT_SECRET,
+  S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+  S3_SECRET_KEY: process.env.S3_SECRET_KEY,
+  S3_BUCKET: process.env.S3_BUCKET,
+  S3_REGION: process.env.S3_REGION,
+  DOCUMENT_MAX_UPLOADS: 10,
+};
 
 console.log(env);
