0d07c6300226a5b809bc7b6f607170897c4fd041
Zackary Fernandez <fernandez.zack@icloud.com>
2024-09-11 22:51:34 -0700

    connection string if running prod for vercel app

--- apps/server/src/db/db.ts
+++ apps/server/src/db/db.ts
@@ -3,10 +3,15 @@ import { env } from "../config";
 
 console.log(env);
 
-export const db = new Pool({
-  user: env.POSTGRES_USER,
-  host: env.POSTGRES_URL,
-  password: env.POSTGRES_PASS,
-  port: 5432,
-  database: env.POSTGRES_DATABASE,
-});
+export const db =
+  env.NODE_ENV == "development"
+    ? new Pool({
+        user: env.POSTGRES_USER,
+        host: env.POSTGRES_URL,
+        password: env.POSTGRES_PASS,
+        port: parseInt(env.POSTGRES_PORT!, 10),
+        database: env.POSTGRES_DATABASE,
+      })
+    : new Pool({
+        connectionString: process.env.POSTGRES_URL,
+      });
