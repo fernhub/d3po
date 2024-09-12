b3480e14f236553ddd0b9f4d6c16d10b9896e9df
Zackary Fernandez <fernandez.zack@icloud.com>
2024-09-12 09:24:02 -0700

    config changes to get deploy working

--- apps/server/src/config.ts
+++ apps/server/src/config.ts
@@ -9,15 +9,13 @@ const { parsed: envs } = result;
 export const env = { ...envs };
 
 Object.assign(env, {
-  NODE_ENV: process.env.NODE_ENV,
   POSTGRES_USER: process.env.POSTGRES_USER,
   POSTGRES_URL: process.env.POSTGRES_URL,
   POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
   POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
-});
-
-Object.assign(env, {
-  BASE_URL: env.NODE_ENV === "development" ? "localhost" : process.env.BASE_URL,
+  API_URL: process.env.API_URL,
+  PORT: process.env.PORT,
+  CLIENT_PORT: process.env.CLIENT_PORT,
 });
 
 console.log(env);
