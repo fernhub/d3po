81ac3cdd0d282bda7da0e6f84572ae4843cfa93f
Zackary Fernandez <fernandez.zack@icloud.com>
2024-09-13 22:34:23 -0700

    embed demo video in homepage

--- apps/client/src/components/UnauthLayout.tsx
+++ apps/client/src/components/UnauthLayout.tsx
@@ -2,6 +2,7 @@ import { Flex, Grid, GridItem, Heading, Spinner } from "@chakra-ui/react";
 import Header from "./Header";
 import { useAtom } from "jotai";
 import { appStateAtom } from "../state/app";
+import ReactPlayer from "react-player";
 
 export default function UnauthLayout() {
   const [appState] = useAtom(appStateAtom);
@@ -24,7 +25,19 @@ export default function UnauthLayout() {
           </Flex>
         ) : (
           <>
-            <div className="demo">Placeholder</div>
+            <div className="video_container">
+              <iframe
+                src="https://www.loom.com/embed/641c7344743f4ab3807feaff0f735c1a?sid=4da0ddf4-09fc-4212-b1bc-094ccf6b95bc&hide_share=true&hideEmbedTopBar=true&hide_title=true&hide_owner=true"
+                className="video_frame"
+                style={{
+                  width: "100%",
+                  minWidth: "100%",
+                  height: "95%",
+                  minHeight: "95%",
+                  marginTop: "5%",
+                }}
+              />
+            </div>
             <Heading className="home-heading">
               Get help interpreting your documents, without the headache of switching LLMs
             </Heading>
